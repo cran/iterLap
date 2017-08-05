1c807af2cada8c41b6473a73b35b8d80ee63c1f3
Bjoern Bornkamp <bbnkmp@gmail.com>
2017-08-05 18:28:28 +0000

    version 1.1-3

---
+++ src/init.c
@@ -0,0 +1,23 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .C calls */
+extern void calcmixmvdens(void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void calcmixtdens(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_CMethodDef CEntries[] = {
+    {"calcmixmvdens", (DL_FUNC) &calcmixmvdens,  9},
+    {"calcmixtdens",  (DL_FUNC) &calcmixtdens,  10},
+    {NULL, NULL, 0}
+};
+
+void R_init_iterLap(DllInfo *dll)
+{
+    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
+
