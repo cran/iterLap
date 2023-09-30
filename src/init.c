4d6444ffae068b6f6788188a4b14dc3e62c49f36
Bjoern Bornkamp <bbnkmp@mail.de>
2023-09-30 22:30:02 +0000

    version 1.1-4

--- src/init.c
+++ src/init.c
@@ -1,10 +1,6 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
-   Check these declarations against the C/Fortran source code.
-*/
-
 /* .C calls */
 extern void calcmixmvdens(void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void calcmixtdens(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
